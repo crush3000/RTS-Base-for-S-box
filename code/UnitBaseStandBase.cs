5787dcec41a95845aa97c214880d8730ac1ddbf0
crush3000 <crush3000>
2024-05-19 10:46:01 -0400

    Unit Base Stands are now decals instead of 3d objects

---
+++ code/UnitBaseStandBase.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox;
+using Sandbox.Citizen;
+using System;
+
+public abstract class UnitBaseStandBase : Component
+{
+	[Property] public Renderer BaseStandModel { get; set; }
+
+	[Property] public UnitModelUtils.OutlineState SelectionOutlineState = UnitModelUtils.OutlineState.Mine;
+
+	public abstract void setOutlineState( UnitModelUtils.OutlineState newOState );
+
+	protected override void OnDestroy()
+	{
+		BaseStandModel.Enabled = false;
+		BaseStandModel.Destroy();
+		base.OnDestroy();
+	}
+}
