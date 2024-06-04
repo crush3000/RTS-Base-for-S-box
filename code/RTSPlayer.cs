7140c059dd87c0fd5690f1597dc5543dabaab4de
crush3000 <crush3000>
2024-06-03 20:45:49 -0400

    Skeltal Spawner is GO

--- code/RTSPlayer.cs
+++ code/RTSPlayer.cs
@@ -5,10 +5,24 @@ public class RTSPlayer : Component
 
 	[Property] public PlayerUnitControl UnitControl;
 	[Property] public int Team;
+	[Property] public GameObject skeltalPrefab {  get; set; }
+
+	private List<GameObject> myUnits = new List<GameObject>();
+	public UnitFactory myUnitFactory = new UnitFactory();
 
 	protected override void OnStart()
 	{
 		base.OnStart();
 		UnitControl = new PlayerUnitControl();
+	}
+
+	public void addUnit(GameObject newUnit)
+	{
+		myUnits.Add( newUnit );
+	}
+
+	public void removeUnit(GameObject unitToRemove)
+	{
+		myUnits.Remove( unitToRemove );
 	}
 }
---
+++ code/UnitFactory.cs
@@ -0,0 +1,17 @@
+﻿public class UnitFactory
+{
+	public void spawnUnit(GameObject unitPrefab, Vector3 newUnitPosition)
+	{
+		if (unitPrefab != null) 
+		{
+			var newUnit = unitPrefab.Clone();
+			newUnit.Transform.Position = newUnitPosition;
+			RTSGame.Instance.ThisPlayer.addUnit( newUnit );
+			newUnit.Enabled = true;
+		}
+		else
+		{
+			Log.Error( "ERROR: Prefab Does Not Exist!" );
+		}
+	}
+}
