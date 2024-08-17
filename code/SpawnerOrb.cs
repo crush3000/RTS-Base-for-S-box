dbe6ba5bba89ceaf8518d6b380746a758a12b631
crush3000 <crush3000>
2024-08-17 14:02:04 -0400

    Forgot to add a bunch of new classes

---
+++ code/SpawnerOrb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class SpawnerOrb : ControlOrb
+{
+	[Property] List<UnitPortraitTuple> spawnableUnitList {  get; set; }
+	protected override void OnStart()
+	{
+		base.OnStart();
+		ThisOrbType = OrbType.Spawner;
+		foreach(var unitType in spawnableUnitList) 
+		{
+			buttons.Add(new ConstructUnitButton('.', unitType.UnitPortraitImage, unitType.UnitPrefab, Transform.Position));
+		}
+	}
+}
---
+++ code/UnitPortraitTuple.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+//This is shite but it will have to do for now because I think I'm gonna have to parse the JSON for each prefab to get the actual string out of it
+public class UnitPortraitTuple : Component
+{
+	[Property] public GameObject UnitPrefab { get; set; }
+
+	[Property] public string UnitPortraitImage { get; set; }
+
+}
